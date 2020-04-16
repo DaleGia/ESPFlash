fd48a4e44f0d843e02791cabf12dc5f6d47a3274
DaleGia <31597257+DaleGia@users.noreply.github.com>
2020-04-16 13:16:57 +0200

    Create ESPFlashString.h

---
+++ src/ESPFlashString.h
@@ -0,0 +1,83 @@
+/*
+ESPFlashString.h
+
+Copyright (c) 2020 Dale Giancono. All rights reserved..
+This file defines the ESPFlashString class. 
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#ifndef ESPFLASHSTRING_H_
+#define ESPFLASHSTRING_H_
+
+#include "ESPFlash.h"
+
+class ESPFlashString
+{
+  public:
+    ESPFlashString(const char* fileName, String defaultString = String());
+    ESPFlashString(const char* fileName, const char* defaultString);
+    void set(String string);
+    void set(const char* string);
+    void reset(void);
+    String get(void);
+  private:
+    String defaultString;
+    ESPFlash<char> espFlash;
+};
+
+ESPFlashString::ESPFlashString(const char* fileName, String defaultString)
+{
+  this->defaultString = defaultString;
+  this->espFlash.setFileName(fileName);
+  if(!SPIFFS.exists(fileName))
+  {
+    set(this->defaultString);
+  }
+}
+
+ESPFlashString::ESPFlashString(const char* fileName, const char* defaultString)
+{
+  this->defaultString = defaultString;
+  this->espFlash.setFileName(fileName);
+  if(!SPIFFS.exists(fileName))
+  {
+    set(this->defaultString);
+  }
+}
+
+void ESPFlashString::set(String string)
+{
+  espFlash.setElements(string.c_str(), string.length());
+}
+
+void ESPFlashString::set(const char* string)
+{
+  espFlash.setElements(string, strlen(string));
+}
+
+void ESPFlashString::reset(void)
+{
+  set(this->defaultString);
+  return;
+}
+
+String ESPFlashString::get(void)
+{
+  char string[espFlash.length()];
+  espFlash.getFrontElements(string, espFlash.length());
+  return String(string);
+}
+
+#endif /*ESPFLASHSTRING_H_*/
